b8e7aa7e5658bf2b2313ea453751ff76c56c6cb1
ishwar ambare <ishuambare@gmail.com>
2020-07-01 22:16:28 +0530

    2 nd commit

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from user.admin import UserCreationForm, UserChangeForm, UserAdmin
+
+
+def user_view(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponse('Hi', user=request.user.get_email_field_name())
+    else:
+        form = UserCreationForm()
+        return render(request, 'UserCreationForm.html', {'form': form})
+
+
+def admin_view(request):
+    pass
